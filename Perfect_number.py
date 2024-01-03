c342dc423785fb18ad43e7d38456eb1c16765334
Tuna MEYDAN <tolexto@hotmail.com>
2024-01-03 18:11:08 +0300

    Update Perfect_number.py
    Addition of comments.

--- Perfect_number.py
+++ Perfect_number.py
@@ -1,34 +1,33 @@
-﻿from ast import Constant
-import colorama
-from colorama import Fore, Back, Style
+﻿import colorama                                                                                                     ## For coloring the output.
+from colorama import Fore
 
-def func1():
+def func1():                                                                                                        ## If the choosen mode is a, gets in this function. This function is checking to only inserted number is perfect or not.
 
-    number = int (input ("insert num: "))
-    multinumbs = []
-    for j in range (number-1):
-        d=number%(j+1)
-        if d == 0:
+    number = int (input ("insert num: "))                                                                           ## User input
+    multinumbs = []                                                                                                 ## Array of multipliers list
+    for j in range (number-1):                                                                                      ## A for loop for divide process. Range is (number-1) because we don't want to check number/number for finding perfect process. Because number/number has no remainder and mixing the calculation steps up.
+        d=number%(j+1)                                                                                              ## Same like above. J is basically 0 if we don't define it. So (j+1) equals 1 in the first step of loop. Every step j increases until reach the number-1. (J+1) = divider. "d" is the mod process.
+        if d == 0:                                                                                                  ## If mod process equals to 0 it means no remainder. the Divider is perfect divider.
             print("Number:", number, "\tDivider:", j+1,"\tRemainder:", d, Fore.GREEN+"\tSuccess!", Fore.WHITE)
-            multinumbs.append(j+1)
-        else:
+            multinumbs.append(j+1)                                                                                  ## We want this dividers. Added to the list.
+        else:                                                                                                       ## If mod process not equals to 0 it means there is a remainder and we don't want this dividers.
             print("Number:", number, "\tDivider:", j+1,"\tRemainder:", d, Fore.RED+"\tNot Success!", Fore.WHITE)
   
-    length = len (multinumbs)
-    multimulti = int(0)
-    print ("exact divisors ", multinumbs)
-    for y in range (length):
-        multimulti = multinumbs[-1] + multimulti
-        multinumbs.pop()
+    length = len (multinumbs)                                                                                       ## Gets length of the list's entities.
+    multimulti = int(0)                                                                                             ## Defining a variable and make it zero. Because zero is effectless in summation process. 
+    print ("exact divisors ", multinumbs)                                                                           ## Printing the members of list before the list getting popped.
+    for y in range (length):                                                                                        ## For loop for getting members out of the array and adding into the "multimulti" variable.
+        multimulti = multinumbs[-1] + multimulti                                                                    ## Picking and adding process.
+        multinumbs.pop()                                                                                            ## Popping last index of array because everytime in the for loop, the loop takes out the last member of the array.
         
-    print ("sum of exact divisors:", multimulti)
+    print ("sum of exact divisors:", multimulti)                                                                    ## Printing the  output.
     
-    if multimulti == number:
-        print(multimulti, "=", number, Fore.GREEN+"Perfect Number!\n", Fore.WHITE+"Let's Try Again!\n\n")
-        main()
+    if multimulti == number:                                                                                        ## CHECKER for the EQUALITY!
+        print(multimulti, "=", number, Fore.GREEN+"Perfect Number!\n", Fore.WHITE+"Let's Try Again!\n\n")           ## Printing out Perfect.
+        main()                                                                                                      ## Goes back to the choosing mode screen.
     else:
-        print(multimulti, "≠", number, Fore.RED+"Not Perfect Bro\n", Fore.WHITE+"Let's Try Again!\n\n")
-        main()
+        print(multimulti, "≠", number, Fore.RED+"Not Perfect Number\n", Fore.WHITE+"Let's Try Again!\n\n")          ## Printing out Not Perfect.
+        main()                                                                                                      ## Goes back to the choosing mode screen.             
 
     
 
@@ -62,7 +61,7 @@ def func2():
         
         
     elif multimulti != number:
-        print(multimulti, "≠", number, Fore.RED+"Not Perfect Bro", Fore.WHITE)
+        print(multimulti, "≠", number, Fore.RED+"Not Perfect Number", Fore.WHITE)
         
     
     print(rangeperfects)   
